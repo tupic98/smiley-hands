176b015547a91de385e861ed962b18eac3df570c
RodrigoA <00017316@uca.edu.sv>
2018-10-11 03:54:55 -0600

    Added all videos and changed design

--- js/main.js
+++ js/main.js
@@ -50,3 +50,53 @@ var slideIndex = 1;
         x[slideIndex2-1].style.display = "block";  
         dots[slideIndex2-1].className += " w3-red";
       }
+
+      $(document).ready(function(){
+        // Add smooth scrolling to all links
+        $("a").on('click', function(event) {
+      
+          // Make sure this.hash has a value before overriding default behavior
+          if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+      
+            // Store hash
+            var hash = this.hash;
+      
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+              scrollTop: $(hash).offset().top
+            }, 800, function(){
+         
+              // Add hash (#) to URL when done scrolling (default click behavior)
+              window.location.hash = hash;
+            });
+          } // End if
+        });
+      });
+      function chbg() {
+        document.getElementsByClassName('protocol_container').style.backgroundColor = "#465da7ad";
+    }   
+      var startPosition =  document.getElementById("Inicio");
+var home_btn =  document.getElementById("HomeBtn");
+
+home_btn.addEventListener("click", scrollToStart);
+window.addEventListener("scroll", homeBtnControl);
+
+function scrollToStart(){
+    window.scroll({
+        top: startPosition,
+        behavior:"smooth"
+    });
+}
+
+function homeBtnControl(){
+    var posY = window.pageYOffset;
+
+    if(posY <= startPosition){
+        home_btn.style.visibility = "hidden";
+    }else{
+        home_btn.style.visibility = "visible";
+    }
+}
